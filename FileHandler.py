5f7d20d40c0a78a536574c34c27e7dac0a00da24
arben777 <arbengb@gmail.com>
2023-09-20 22:18:43 -0700

    further edits to allow any columns in contact sheets, and removal of placeholder function

--- FileHandler.py
+++ FileHandler.py
@@ -1,4 +1,5 @@
-from Contact import Contact , CategorizedContact
+
+from Contact import Contact, CategorizedContact
 import pandas as pd
 import os
 from typing import List
@@ -8,25 +9,35 @@ class FileHandler:
     @staticmethod
     def read_files(directory: str) -> List[Contact]:
         data = []
-        for file in os.listdir(directory):
-            if file.endswith(('.csv', '.xls', '.xlsx')):
-                path = os.path.join(directory, file)
-                df = pd.read_csv(path) if file.endswith('.csv') else pd.read_excel(path)
-                for _, row in df.iterrows():
-                    contact = Contact(**row.to_dict())
-                    data.append(contact)
+        try:
+            for file in os.listdir(directory):
+                if file.endswith(('.csv', '.xls', '.xlsx')):
+                    path = os.path.join(directory, file)
+                    try:
+                        df = pd.read_csv(path) if file.endswith('.csv') else pd.read_excel(path)
+                        for _, row in df.iterrows():
+                            contact = Contact(**row.to_dict())
+                            data.append(contact)
+                    except Exception as e:
+                        print(f"Error reading file {file}: {e}")
+        except IOError as e:
+            print(f"Error reading files from directory {directory}: {e}")
         return data
 
     @staticmethod
     def write_to_excel(categorized_contacts: List[CategorizedContact], output_file: str):
-        categorized_data = {}
-        for contact in categorized_contacts:
-            category = contact.industry
-            if category not in categorized_data:
-                categorized_data[category] = []
-            categorized_data[category].append(contact.contact.__dict__)
+        try:
+            categorized_data = {}
+            for contact in categorized_contacts:
+                category = contact.industry
+                if category not in categorized_data:
+                    categorized_data[category] = []
+                categorized_data[category].append(contact.contact.__dict__)
 
-        with pd.ExcelWriter(output_file) as writer:
-            for category, contacts in categorized_data.items():
-                df = pd.DataFrame(contacts)
-                df.to_excel(writer, sheet_name=category, index=False)
+            with pd.ExcelWriter(output_file) as writer:
+                for category, contacts in categorized_data.items():
+                    df = pd.DataFrame(contacts)
+                    df.to_excel(writer, sheet_name=category, index=False)
+        except IOError as e:
+            print(f"Error writing to Excel file {output_file}: {e}")
+
--- OpenAICaller.py
+++ OpenAICaller.py
@@ -8,23 +8,9 @@ import json
 import random
 
 class OpenAICaller:
-
-    @staticmethod
-    def categorize_contact(contact: Contact) -> CategorizedContact:
-        # Mock GPT function call to get categorized attributes
-        # In a real-world scenario, you'd replace this with the actual GPT function call
-        response = {
-            'industry': random.choice(["Tech", "Finance", "Healthcare", "Entertainment"]),
-            'organization': random.choice(["OrgA", "OrgB", "OrgC", "OrgD"]),
-            'seniority': random.randint(1, 10)
-        }
-        
-        # Create and return a CategorizedContact object
-        return CategorizedContact(contact, response['industry'], response['organization'], response['seniority'])
-
 # Load OpenAI key
-load_dotenv()
-openai.api_key = os.getenv('OPEN_AI_KEY')
+    load_dotenv()
+    openai.api_key = os.getenv('OPEN_AI_KEY')
 
 @staticmethod
 def categorize_contact(contact: Contact) -> CategorizedContact:
